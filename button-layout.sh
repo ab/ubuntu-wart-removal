c9dce17bb7ac7968e7e63924d530c803ab191ab3
Andy Brody <andy@abrody.com>
2012-04-07 04:11:59 -0700

    Add right-nomenu option.

--- button-layout.sh
+++ button-layout.sh
@@ -2,18 +2,25 @@
 # move buttons right or left
 
 if [ $# -lt 1 ]; then
-    echo "$(basename "$0") [right|left]"
+    echo "$(basename "$0") [right|nomenu|left]"
     exit 1
 fi
 
-if [ "$1" = "right" ]; then
-    layout="menu:minimize,maximize,close"
-elif [ "$1" = "left" ]; then
-    layout="close,minimize,maximize:"
-else
-    echo "unknown layout"
-    exit 2
-fi
+case "$1" in
+    right)
+        layout="menu:minimize,maximize,close"
+        ;;
+    nomenu)
+        layout=":minimize,maximize,close"
+        ;;
+    left)
+        layout="close,minimize,maximize:"
+        ;;
+    *)
+        echo "unknown layout"
+        exit 2
+        ;;
+esac
 
 echo "current: '$(gconftool -g /apps/metacity/general/button_layout)'"
 echo "target:  '$(gconftool -g /apps/metacity/general/button_layout)'"
