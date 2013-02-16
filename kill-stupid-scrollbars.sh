9d9045e3f1a929f4f800b2c96bd28366298645e7
Andy Brody <andy@stripe.com>
2013-02-15 19:15:21 -0800

    Use newer method to kill overlay scrollbars.

--- kill-stupid-scrollbars.sh
+++ kill-stupid-scrollbars.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 set -eux
 
-sudo aptitude remove overlay-scrollbar liboverlay-scrollbar3-0.2-0 liboverlay-scrollbar-0.2-0
+# http://askubuntu.com/questions/34214/how-do-i-disable-overlay-scrollbars
+
+#sudo aptitude remove overlay-scrollbar liboverlay-scrollbar3-0.2-0 liboverlay-scrollbar-0.2-0
+echo 'export LIBOVERLAY_SCROLLBAR=0' >> ~/.xprofile
