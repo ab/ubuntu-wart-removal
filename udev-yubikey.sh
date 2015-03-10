9f192619c237303a96d66c2253ac2e2bad0ea0fb
Andy Brody <andy@stripe.com>
2015-03-10 13:54:45 -0700

    Add rules for OTP/CCID capable yubikeys.

--- udev-yubikey.sh
+++ udev-yubikey.sh
@@ -1,45 +1,62 @@
 #!/bin/bash
 set -eu
 
-source_file="$(dirname "$0")/data/70-u2f.rules"
-content="$(cat "$source_file")"
+if which colordiff >/dev/null; then
+    DIFF=colordiff
+else
+    DIFF=diff
+fi
+
+run() {
+    echo >&2 "+ $*"
+    "$@"
+}
+
+diff_cp() {
+    local src dst
+    if [ $# -lt 2 ]; then
+        echo >&2 "do_compare SOURCE DEST"
+        return 1
+    fi
+    src="$1"
+    dst="$2"
+
+    if [ -e "$dst" ]; then
+        "$DIFF" -u -- "$src" "$dst" && ret=$? || ret=$?
+
+        case "$ret" in
+            0)
+                echo "$dst already installed"
+                return 0
+                ;;
+            1)
+                # there will be changes
+                ;;
+            *)
+                echo >&2 "$DIFF exited with status $ret"
+                return "$ret"
+                ;;
+        esac
+    fi
+
+    run sudo cp -iv -- "$src" "$dst"
+}
+
+u2f_src="$(dirname "$0")/data/70-u2f.rules"
+content="$(cat "$u2f_src")"
 
 url=https://raw.githubusercontent.com/Yubico/libu2f-host/master/70-u2f.rules
 web_content="$(wget -O - "$url")"
 
-echo "Checking that our saved data at $source_file is up to date from $url"
+echo "Checking that our saved data at $u2f_src is up to date from $url"
 
 if [ "$web_content" != "$content" ]; then
-    echo >&2 "Error: content from web at $url differs from $source_file"
+    echo >&2 "Error: content from web at $url differs from $u2f_src"
     exit 1
 fi
 
-target=/etc/udev/rules.d/70-u2f.rules
+diff_cp "$u2f_src" /etc/udev/rules.d/70-u2f.rules
 
-if which colordiff >/dev/null; then
-    diff=colordiff
-else
-    diff=diff
-fi
+yubikey_src="$(dirname "$0")/data/68-yubikey.rules"
 
-if [ -e "$target" ]; then
-    "$diff" -u -- "$source_file" "$target" && ret=$? || ret=$?
-
-    case "$ret" in
-        0)
-            echo "$target already installed"
-            exit 0
-            ;;
-        1)
-            # there will be changes
-            ;;
-        *)
-            echo >&2 "$diff exited with status $ret"
-            exit "$ret"
-            ;;
-    esac
-fi
-
-set -x
-
-sudo cp -iv -- "$source_file" "$target"
+diff_cp "$yubikey_src" /etc/udev/rules.d/68-yubikey.rules
